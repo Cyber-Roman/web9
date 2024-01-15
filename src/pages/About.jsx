6c9ebdcae157cf09c08312a713391ead6d14f990
Roman Dulin <63359306+Cyber-Roman@users.noreply.github.com>
2024-01-15 14:11:49 +0200

    Add files via upload

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+// About.jsx
+import React from 'react';
+import ParentComponent from './ParentComponent';
+
+function About() {
+  return (
+    <div>
+      <h2>Сторінка "Про нас"</h2>
+      <div>
+      <h1>React CSSTransitions Example</h1>
+      <ParentComponent />
+      <ParentComponent/>
+    </div>
+    </div>
+  );
+}
+
+export default About;
